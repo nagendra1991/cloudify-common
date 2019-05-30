c9d44083c8c0524bc4d4e8a373337b4833a35216
Lukasz Maksymczuk <tehasdf@users.noreply.github.com>
2019-05-30 13:48:15 +0200

    CY-1321 Update remote operation state on the dispatch side (#307)
    For remote tasks, it is the agent that is updating the operation
    state. This way, the state will be updated even if the workflow
    was cancelled.
    Additionally, TASK_STARTED state now makes sense as well!

--- cloudify/workflows/workflow_context.py
+++ cloudify/workflows/workflow_context.py
@@ -1270,14 +1270,13 @@ class _TaskDispatcher(object):
         ping_handler = amqp_client.BlockingRequestResponseHandler(
             exchange=task['target'])
         client = self._get_client(task)
-        client.add_handler(handler)
         client.add_handler(ping_handler)
+        client.add_handler(handler)
         task.update({
             'client': client,
             'handler': handler,
             'ping_handler': ping_handler
         })
-        client.consume_in_thread()
         return task
 
     def _get_client(self, task):
@@ -1318,6 +1317,7 @@ class _TaskDispatcher(object):
 
         self._tasks.setdefault(client, {})[task['id']] = \
             (workflow_task, task, result)
+        client.consume_in_thread()
         return result
 
     def _set_task_state(self, workflow_task, state, event=None):
